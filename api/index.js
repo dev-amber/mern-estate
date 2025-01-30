7f5249140c3953a0282ba9ff4dcce8a64d9143fb
dev-amber <ambernaz1000@gmail.com>
2025-01-30 18:41:45 +0500

    create user update api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv"
 import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
+import cookieParser from "cookie-parser";
+
 dotenv.config();
 
 
@@ -18,7 +20,8 @@ const app=express();
 
 app.use(express.json());
 
-
+//cookie parser
+app.use(cookieParser());
 
 
 
