12de6963d59644499d87bd402353dfeff92c2895
dev-amber <ambernaz1000@gmail.com>
2025-03-13 22:02:55 +0500

    complete update listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -69,3 +69,16 @@ export const updateListing=async(req,res,next)=>{
   }
   
 }
+
+//getLIsting
+export const getListing=async(req,res,next)=>{
+  try {
+    const listing=await Listing.findById(req.params.id);
+    if(!listing){
+      return next(errorHandler(401,"Listing not found"));
+    }
+    res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
+}
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -4,6 +4,7 @@ import {
   getSingleListing,
   deleteListing,
   updateListing,
+  getListing,
   
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
@@ -13,6 +14,7 @@ const router = express.Router();
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id",verifyToken,deleteListing);
 router.post("/update/:id",verifyToken,updateListing);
+router.get("/get/:id",getListing);
 router.get("/get/:id", getSingleListing);
 
 export default router;
