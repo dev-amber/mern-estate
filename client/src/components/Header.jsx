ddf34da30e4572bd30a69f13f776a117d13b437b
imtahirabatool <btahira183@gmail.com>
2025-04-19 17:36:41 +0500

    /route issue resolved

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,23 +5,24 @@ import { useEffect, useState } from "react";
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
-  const [searchTerm,setSearchTerm]= useState("");
-  const navigate=useNavigate();
+  const [searchTerm, setSearchTerm] = useState("");
+  const [formData, setFormData] = useState({});
+  const navigate = useNavigate();
 
-const handleSubmit=(e)=>{
-  e.preventDefault();
-  const urlParams=new URLSearchParams(window.location.search);
-  urlParams.set("searchTerm",searchTerm);
-  const searchQuery=urlParams.toString();
-  navigate(`/search/${searchQuery}`);
-}
-useEffect(()=>{
-  const urlParams=new URLSearchParams(location.search);
-const searchTermFormUrl=urlParams.get("searchTerm");
-if(searchTermFormUrl){
-  setSearchTerm(searchTermFormUrl);
-}
-},[location.search]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFormUrl = urlParams.get("searchTerm");
+    if (searchTermFormUrl) {
+      setSearchTerm(searchTermFormUrl);
+    }
+  }, [location.search]);
 
   return (
     <header className="bg-slate-200 shadow-md">
@@ -32,19 +33,20 @@ if(searchTermFormUrl){
             <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
-        <form  onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
           <input
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
-             value={searchTerm}
-             onChange={(e)=>setSearchTerm(e.target.value)}
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
           <button>
-            
-          <FaSearch className="text-slate-600" />
+            <FaSearch className="text-slate-600" />
           </button>
-         
         </form>
         <ul className="flex gap-4">
           <Link to="/">
@@ -61,7 +63,7 @@ if(searchTermFormUrl){
             {currentUser ? (
               <img
                 className="rounded-full h-7 w-7  object-cover"
-                src={currentUser.avatar}
+                src={formData?.avatar || currentUser.avatar}
                 alt="profile"
               />
             ) : (
